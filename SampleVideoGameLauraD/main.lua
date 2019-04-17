4a446102b08986dba6a645647f080e1bb85dd24a
s269431 <47784134+s269431@users.noreply.github.com>
2019-04-17 08:58:27 -0400

    Initial commit

---
+++ SampleVideoGameLauraD/main.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+-- calls splash screen
+composer.gotoScene ("splash_screen")
